00dbf1fd96b6cf0e8d7e8e212d4884de8d05c16e
joon <pirxthepilot@users.noreply.github.com>
2023-08-09 00:13:37 -0700

    Revise colors and move hyperlinks

--- wtfis/ui/theme.py
+++ wtfis/ui/theme.py
@@ -1,7 +1,7 @@
 class Theme:
     panel_title_default = "default"
     panel_title_main = "bold yellow"
-    heading_h1 = "bold black on green"
+    heading_h1 = "bold bright_green on dark_green"
     heading_h2 = "bold yellow"
     table_field = "bold bright_magenta"
     table_value = "none"
